67e53a7740025a5a41127e83991465d263145535
lucasfiasco <lucas.godek@gmail.com>
2026-01-08 15:29:47 +0000

    cmakegit add .

---
+++ build/CMakeFiles/zero.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zero.dir/link.d"
+  "CMakeFiles/zero.dir/src/main.cpp.o"
+  "CMakeFiles/zero.dir/src/main.cpp.o.d"
+  "zero"
+  "zero.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zero.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- test.asm
+++ test.asm
@@ -1,1 +1,6 @@
-// test
+global _start
+_start:
+    mov rax, 60
+    mov rdi, 60
+    syscall
+    
